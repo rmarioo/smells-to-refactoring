3ac18f67c2faf8f10b87c90bd48b7d34478ca4f6
mrusso <mario.russo@rumbo.com>
2016-12-17 13:05:09 +0100

    better test names , and removed few duplication

--- conditionals/src/test/java/it/rmarioo/smells_to_refactoring/conditionals/CarTest.java
+++ conditionals/src/test/java/it/rmarioo/smells_to_refactoring/conditionals/CarTest.java
@@ -8,31 +8,31 @@ import static org.junit.Assert.*;
 
 public class CarTest
 {
+  private final Car miniCar = new Car("Hyundai i10", MINI);
+  private final Car economyCar = new Car("Ford Fiesta", ECONOMY);
+  private final Car luxuryCar = new Car("BMW", LUXURY);
+
   @Test
-  public void mini() throws Exception
+  public void miniCarRental_lessThan_10_days() throws Exception
   {
-    Car car = new Car("Hyundai i10", MINI);
-    assertThat(car.amountForDays(5), is(20));
+    assertThat(miniCar.amountForDays(5), is(20));
   }
 
   @Test
-  public void mini_longRental() throws Exception
+  public void miniCarRental_moreThan_10_days() throws Exception
   {
-    Car car = new Car("Hyundai i10", MINI);
-    assertThat(car.amountForDays(15), is(35));
+    assertThat(miniCar.amountForDays(15), is(35));
   }
 
   @Test
-  public void economy() throws Exception
+  public void economyCarRental() throws Exception
   {
-    Car car = new Car("Ford Fiesta", ECONOMY);
-    assertThat(car.amountForDays(5), is(35));
+    assertThat(economyCar.amountForDays(5), is(35));
   }
 
   @Test
-  public void luxury() throws Exception
+  public void luxuryCarRental() throws Exception
   {
-    Car car = new Car("BMW", LUXURY);
-    assertThat(car.amountForDays(5), is(75));
+    assertThat(luxuryCar.amountForDays(5), is(75));
   }
 }
