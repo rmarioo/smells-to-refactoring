793f751e5e09a26c57011d40b65f67deaa7c9cc5
mrusso <mario.russo@rumbo.com>
2016-12-17 13:07:16 +0100

    moving constructions in tests

--- conditionals/src/test/java/it/rmarioo/smells_to_refactoring/conditionals/CarTest.java
+++ conditionals/src/test/java/it/rmarioo/smells_to_refactoring/conditionals/CarTest.java
@@ -8,31 +8,32 @@ import static org.junit.Assert.*;
 
 public class CarTest
 {
-  private final Car miniCar = new Car("Hyundai i10", MINI);
-  private final Car economyCar = new Car("Ford Fiesta", ECONOMY);
-  private final Car luxuryCar = new Car("BMW", LUXURY);
 
   @Test
   public void miniCarRental_lessThan_10_days() throws Exception
   {
-    assertThat(miniCar.amountForDays(5), is(20));
+    Car car = new Car("Hyundai i10", MINI);
+    assertThat(car.amountForDays(5), is(20));
   }
 
   @Test
   public void miniCarRental_moreThan_10_days() throws Exception
   {
-    assertThat(miniCar.amountForDays(15), is(35));
+    Car car = new Car("Hyundai i10", MINI);
+    assertThat(car.amountForDays(15), is(35));
   }
 
   @Test
   public void economyCarRental() throws Exception
   {
-    assertThat(economyCar.amountForDays(5), is(35));
+    Car car = new Car("Ford Fiesta", ECONOMY);
+    assertThat(car.amountForDays(5), is(35));
   }
 
   @Test
   public void luxuryCarRental() throws Exception
   {
-    assertThat(luxuryCar.amountForDays(5), is(75));
+    Car car = new Car("BMW", LUXURY);
+    assertThat(car.amountForDays(5), is(75));
   }
 }
