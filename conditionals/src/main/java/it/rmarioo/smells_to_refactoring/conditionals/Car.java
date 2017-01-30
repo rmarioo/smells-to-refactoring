c395e06cf4842e308faee01f628d4e6b3e9784b4
mrusso <mario.russo@rumbo.com>
2017-01-30 21:24:51 +0100

    push amount calculation in subclasses by "push member down"

--- conditionals/src/main/java/it/rmarioo/smells_to_refactoring/conditionals/Car.java
+++ conditionals/src/main/java/it/rmarioo/smells_to_refactoring/conditionals/Car.java
@@ -1,13 +1,13 @@
 package it.rmarioo.smells_to_refactoring.conditionals;
 
-public class Car
+public abstract class Car
 {
   public static final int MINI = 0;
   public static final int ECONOMY = 1;
   public static final int LUXURY = 2;
 
   private String name;
-  private int type;
+  protected int type;
 
   public Car(String name, int type)
   {
@@ -15,25 +15,6 @@ public class Car
     this.type = type;
   }
 
-  public int amountForDays(int daysRented)
-  {
-    int thisAmount = 0;
-    switch(type)
-    {
-      case MINI:
-        if (daysRented < 10)
-          thisAmount = 10 + (daysRented * 2);
-        else
-          thisAmount = 20 + (daysRented * 1);
-        break;
-      case ECONOMY:
-        thisAmount = 20 + (daysRented * 3);
-        break;
-      case LUXURY:
-        thisAmount = 50 + (daysRented * 5);
-        break;
-    }
-    return thisAmount;
-  }
+  public abstract int amountForDays(int daysRented);
 
 }
--- conditionals/src/main/java/it/rmarioo/smells_to_refactoring/conditionals/EconomyCar.java
+++ conditionals/src/main/java/it/rmarioo/smells_to_refactoring/conditionals/EconomyCar.java
@@ -6,4 +6,26 @@ public class EconomyCar extends Car
   {
     super(name,ECONOMY);
   }
+
+  @Override
+  public int amountForDays(int daysRented)
+  {
+    int thisAmount = 0;
+    switch(type)
+    {
+      case MINI:
+        if (daysRented < 10)
+          thisAmount = 10 + (daysRented * 2);
+        else
+          thisAmount = 20 + (daysRented * 1);
+        break;
+      case ECONOMY:
+        thisAmount = 20 + (daysRented * 3);
+        break;
+      case LUXURY:
+        thisAmount = 50 + (daysRented * 5);
+        break;
+    }
+    return thisAmount;
+  }
 }
