5c0a45c22d32563ba8f19f0ed65d105ebb3e044c
mrusso <mario.russo@rumbo.com>
2017-01-30 21:10:51 +0100

    removed useless throws to save space

--- conditionals/src/test/java/it/rmarioo/smells_to_refactoring/conditionals/CarTest.java
+++ conditionals/src/test/java/it/rmarioo/smells_to_refactoring/conditionals/CarTest.java
@@ -8,30 +8,29 @@ import static org.junit.Assert.*;
 
 public class CarTest
 {
-
   @Test
-  public void miniCarRental_lessThan_10_days() throws Exception
+  public void miniCarRental_lessThan_10_days()
   {
     Car car = new Car("Hyundai i10", MINI);
     assertThat(car.amountForDays(5), is(20));
   }
 
   @Test
-  public void miniCarRental_moreThan_10_days() throws Exception
+  public void miniCarRental_moreThan_10_days()
   {
     Car car = new Car("Hyundai i10", MINI);
     assertThat(car.amountForDays(15), is(35));
   }
 
   @Test
-  public void economyCarRental() throws Exception
+  public void economyCarRental()
   {
     Car car = new Car("Ford Fiesta", ECONOMY);
     assertThat(car.amountForDays(5), is(35));
   }
 
   @Test
-  public void luxuryCarRental() throws Exception
+  public void luxuryCarRental()
   {
     Car car = new Car("BMW", LUXURY);
     assertThat(car.amountForDays(5), is(75));
